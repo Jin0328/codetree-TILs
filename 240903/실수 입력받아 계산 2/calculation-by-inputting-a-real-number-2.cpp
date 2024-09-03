f416dc619df2b32ef936eef932b6281918f46219
Jin0328 <144994590+Jin0328@users.noreply.github.com>
2024-09-03 15:29:27 +0900

    Title: 실수 입력받아 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240903/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
+++ 240903/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     double a;
+    cin >> a;
     
     cout << fixed;
     cout.precision(2);
 
-    cout << 1.5 + a << endl;
+    cout << a + 1.5 << endl;
     return 0;
 }
