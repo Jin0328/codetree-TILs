22779fd7ac8a5085b63692ffcb9d2ed3a867f23e
Jin0328 <144994590+Jin0328@users.noreply.github.com>
2024-09-05 23:53:05 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240905/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+
+    cin >> a;
+    cin.get();
+    cin >> b;
+
+    cout << a << b << endl;
+    return 0;
+}
