3579d237b59964b0b7effa46402dd3b542ef4a92
Jin0328 <144994590+Jin0328@users.noreply.github.com>
2024-09-09 11:01:07 +0900

    Title: 합과 차의 나눗셈, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240909/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    double c;
+
+    cin >> a >> b;
+    c = double(a + b) / (a - b);
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << c << endl;
+    return 0;
+}
