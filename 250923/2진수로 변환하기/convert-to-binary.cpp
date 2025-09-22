af219cf238475ef19f6dcba0e0aae2a13f9b1d0c
Jiin <144994590+Jin0328@users.noreply.github.com>
2025-09-23 07:55:51 +0900

    Title: 2진수로 변환하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250923/2진수로 변환하기/convert-to-binary.cpp
+++ 250923/2진수로 변환하기/convert-to-binary.cpp
@@ -10,12 +10,15 @@ int main() {
     // Please write your code here.
     int digits[20];
     int cnt = 0;
-    while(n > 2){
+    while(true){
+        if(n < 2){
+            digits[cnt++] = n;
+            break;
+        }
         digits[cnt++] = n % 2;
         n /= 2;
     }
 
-    digits[cnt++] = n;
 
     for(int i = cnt - 1; i >= 0; i--){
         cout << digits[i];
