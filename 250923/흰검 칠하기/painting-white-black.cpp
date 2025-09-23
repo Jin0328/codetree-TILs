d1ea35077a2262e05fe29799bc4241d11cd5db2d
Jiin <144994590+Jin0328@users.noreply.github.com>
2025-09-23 14:09:32 +0900

    Title: 흰검 칠하기, Time: 10ms, Memory: 2MB, Status: Passed - Codetree

--- 250923/흰검 칠하기/painting-white-black.cpp
+++ 250923/흰검 칠하기/painting-white-black.cpp
@@ -15,35 +15,31 @@ int main() {
 
     int pos = INF;
     vector<int> line(INF * 2, -1);
-    vector<bool> is_visited_w(INF * 2, false);
+    vector<int> is_visited_w(INF * 2, 0);
+    vector<int> is_visited_b(INF * 2, 0);
 
     while(n--){
         cin >> size >> dir;
         // 흰색으로 색칠
         // 필요한 것 : 인덱스, 색깔, 칠해진 수
         if(dir == 'L'){
-            for(int i = pos - size; i < pos; i++){
-                if(is_visited[i] == true){
+            for(int i = pos - size + 1; i <= pos; i++){
+                line[i] = 0; // 흰색
+                is_visited_w[i]++;
+                if(is_visited_w[i] > 1 && is_visited_b[i] > 1){
                     line[i] = 2; // 회색
-                } else{
-                    line[i] = 0; // 흰색
-                    is_visited[i] = true;
-                }
-                cout << i << " " << line[i] << endl;
-                
+                }       
             }
-            pos -= size;
+            pos = pos - size + 1;
         } else {
             for(int i = pos; i < pos + size; i++){
-                if(is_visited[i] == true){
+                line[i] = 1; // 검은색
+                is_visited_b[i]++;
+                if(is_visited_w[i] > 1 && is_visited_b[i] > 1){
                     line[i] = 2; // 회색
-                } else{
-                    line[i] = 1; // 검은색
-                    is_visited[i] = true;
                 }
-                cout << i << " " << line[i] << endl;
             }
-            pos += size;
+            pos = pos + size - 1;
         }
     }
 
