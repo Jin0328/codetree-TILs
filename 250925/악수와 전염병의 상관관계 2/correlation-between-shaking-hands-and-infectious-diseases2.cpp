a8e27e3e92d75342c1f633065427ff106de4602e
Jiin <144994590+Jin0328@users.noreply.github.com>
2025-09-25 02:13:37 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250925/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Handshake{
+    int t, x, y;
+};
+
+bool cmp(Handshake &a, Handshake &b){
+    return a.t < b.t;
+}
+
+int main() {
+    int N, K, P, T;
+    cin >> N >> K >> P >> T;
+
+    vector<Handshake> list(T);
+    for(int i = 0; i < T; i++){
+        cin >> list[i].t >> list[i].x >> list[i].y;
+    }
+
+    sort(list.begin(), list.end(), cmp);
+    
+    vector<bool> infected(N + 1, false);
+    vector<int> cnt(N + 1, 0);
+
+    infected[P] = true;
+    for(auto iter : list){
+        int x = iter.x;
+        int y = iter.y;
+
+        if(infected[x] && !infected[y] && (cnt[x] < K)){
+            infected[y] = true;
+            cnt[x]++;
+        }
+
+        if(infected[y] && !infected[x] && (cnt[y] < K)){
+            infected[y] = true;
+            cnt[y]++;
+        }
+    }
+
+    for(int i = 1; i <= N; i++){
+        if(infected[i]) cout << 1;
+        else cout << 0;
+    }
+    
+    return 0;
+}
