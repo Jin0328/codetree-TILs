689c1c1f7ce6b0eec6243f84cc4b677f0477a6bd
Jiin <144994590+Jin0328@users.noreply.github.com>
2025-09-26 01:16:12 +0900

    Title: 마을 구분하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250926/마을 구분하기/seperate-village.cpp
+++ 250926/마을 구분하기/seperate-village.cpp
@@ -9,7 +9,6 @@ int grid[25][25];
 int dx[4] = {0, 0, -1, 1};
 int dy[4] = {1, -1, 0, 0};
 bool visited[25][25] = {false, };
-int cnt = 1;
 int village = 0;
 vector<int> people;
 
@@ -27,13 +26,13 @@ bool canGo(int x, int y){
 
 // 마을 하나의 사람 수를 구함
 int dfs(int x, int y){
+    int cnt = 1;
     for(int i = 0; i < 4; i++){
         int new_x = x + dx[i];
         int new_y = y + dy[i];
         if(!visited[new_x][new_y] && canGo(new_x, new_y)){
             visited[new_x][new_y] = true;
-            cnt++;
-            dfs(new_x, new_y);
+            cnt += dfs(new_x, new_y);
         }
     }
     return cnt;
@@ -55,7 +54,6 @@ int main() {
                 visited[i][j] = true;
                 people.push_back(dfs(i, j));
                 village++;
-                cnt = 1;
             }
         }
     }
